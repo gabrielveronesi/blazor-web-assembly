b5f18ee19544e183d19a9ac3fdf64f2e4a335f2d
Gabriel Veronesi <57780382+gabrielveronesi@users.noreply.github.com>
2023-07-21 16:48:19 -0300

    Enviando só pra 1 numero

--- Program.cs
+++ Program.cs
@@ -2,12 +2,11 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using blazor_web_assembly;
 using RestSharp;
-using HttpClient;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddHttpClient();
+
 builder.Services.AddSingleton(new RestClient(new HttpClient()));
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
